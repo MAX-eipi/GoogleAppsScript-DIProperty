c1b64440aeb7e521637e6667152b6e76e86ee958
MAX-eipi <max.eipi.3141@gmail.com>
2021-01-03 16:56:11 +0900

    update

--- DIProperty.ts
+++ DIProperty.ts
@@ -2,29 +2,65 @@ export class DIProperty {
     private static readonly _factories: Record<string, Record<string, () => any>> = {};
     private static readonly _container: Record<string, Record<string, any>> = {};
 
-    public static bind<T>(key: Function, factory: () => T, groupKey = 'global'): void {
+    private static toInternalKey(key: string | Function): string {
+        return key instanceof Function ? key.name : key;
+    }
+
+    private static getFactories(groupKey: string): Record<string, () => any> {
         if (!(groupKey in this._factories)) {
             this._factories[groupKey] = {};
         }
-        this._factories[groupKey][key.name] = factory;
+        return this._factories[groupKey];
     }
 
-    public static inject(target: Function, groupKey = 'global'): any {
+    private static getContainer(groupKey: string): Record<string, any> {
+        if (!(groupKey in this._container)) {
+            this._container[groupKey] = {};
+        }
+        return this._container[groupKey];
+    }
+
+    public static bind<T>(key: string | Function, factory: () => T, groupKey = 'global'): void {
+        this.getFactories(groupKey)[this.toInternalKey(key)] = factory;
+    }
+
+    public static register<T>(key: string | Function, instance: T, groupKey = 'global'): void {
+        this.getContainer(groupKey)[this.toInternalKey(key)] = instance;
+    }
+
+    private static resolve(key: string | Function, groupKey = 'global'): any {
+        const internalKey = this.toInternalKey(key);
+        const container = this.getContainer(groupKey);
+        if (!(internalKey in container)) {
+            container[internalKey] = this.getFactories(groupKey)[internalKey];
+        }
+        return container[internalKey];
+    }
+
+    public static inject(target: string | Function, groupKey = 'global'): any {
         const self = this;
         return function (_target: any, propertyKey: string): PropertyDescriptor {
             return {
                 configurable: false,
                 enumerable: false,
                 get: () => {
-                    if (!(groupKey in self._container)) {
-                        self._container[groupKey] = {};
-                    }
-                    if (!(target.name in self._container[groupKey])) {
-                        self._container[groupKey][target.name] = self._factories[groupKey][target.name]();
-                    }
-                    return self._container[groupKey][target.name]
+                    return self.resolve(target, groupKey);
                 }
             }
         }
+    }
+
+    public static createKey(...keys: (string | Function)[]): string {
+        let ret = "";
+        for (const key of keys) {
+            const _key = this.toInternalKey(key);
+            if (ret) {
+                ret = ret + _key;
+            }
+            else {
+                ret = _key;
+            }
+        }
+        return ret;
     }
 }
